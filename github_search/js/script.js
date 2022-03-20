2eb887d8ca97c7625f81be0df2de97f24781417c
shamsurie <mdshamsurie@gmail.com>
2022-03-21 00:18:23 +0800

    simple github search

--- github_search/js/script.js
+++ github_search/js/script.js
@@ -1,14 +1,43 @@
-$('#search-button').on('click', function(){
 
+$('#search-button').on('click', function(){
+   
     $.ajax({
         url: 'https://api.github.com/search/repositories',
         type: 'get',
         dataType: 'jsonp',
         data:{
-            'q':'',
-            
+            'q': $('#search-input').val(),
+        },
+        success: function (res) {
+            res = res.data.items;
+              //   console.log(res.items);
 
+              $.each(res, function (i, data){
+
+                $('#github-list').append(`<div class="col-md-4 mt-3"><div class="card h-100"> <div class="card-body">
+                <h5 class="card-title">` + data.full_name + `</h5>
+                <p class="card-text">` + data.description + `</p>
+                <a href="` + data.html_url + `" class="btn btn-primary float-right h-20">Go to github</a>
+                </div>
+                </div>
+                </div>
+                    
+                
+                `)
+              });
+            // if( res.incomplete_results === 'false'){
+
+            //     let data = res.data.items;
+            //     console.log(data);
+            // }
+            // else{
+
+            //     $('#message').html(`<h1 class="text-center"> Invalid Searching.... </h1>`);
+            // }
+        
         }
-    })
+    });
 
-})
+  
+
+});
